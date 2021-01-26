ea9bdb70336501ed9454030aeca0d0af96d43667
Pavel Kuzminov <minecraftpavel@gmail.com>
2021-01-26 09:19:50 +0100

    Added launcher and updater for clients

---
+++ Laucher.ps1
@@ -0,0 +1,33 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {  
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+Write-Host "Kollar om du har PowerShell7"
+if (!(Test-Path .\pwsh\pwsh.exe)) {
+    Write-Host "PowerShell saknas, laddar ner (Detta kan ta en stund)"
+    Copy-Item -Path '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\pwsh' -Destination .\ -Force -Recurse
+}
+Write-Host "Kollar om du har startfilen eller om det finns en uppdatering"
+if (!(Test-Path ./HittaDator.bat)) {
+    Write-Host "Filen saknas, laddar ner"
+    Copy-Item -Path '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\HittaDator.bat' -Destination .\HittaDator.bat -Force
+}
+elseif(!(Get-FileHash .\HittaDator.bat).Hash -eq (Get-FileHash '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\HittaDator.bat').Hash)
+{
+    Write-Host "Uppdatering hittad, laddar ner"
+    Copy-Item -Path '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\HittaDator.bat' -Destination .\HittaDator.bat -Force
+}
+Write-Host "Kollar om du har scriptet eller om det finns en uppdatering"
+if (!(Test-Path .\FindPC.ps1)) {
+    Write-Host "Scriptet saknas, laddar ner"
+    Copy-Item -Path '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\FindPC.ps1' -Destination .\FindPC.ps1 -Force
+}
+elseif(!(Get-FileHash .\FindPC.ps1).Hash -eq (Get-FileHash '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\FindPC.ps1').Hash)
+{
+    Write-Host "Uppdatering hittad, laddar ner"
+    Copy-Item -Path '\\dfs\Gem$\Lit\IT-Service\G55\HittaPC\FindPC.ps1' -Destination .\FindPC.ps1 -Force
+}
+Write-Host "Startar Scriptet"
+# Launch the (now possibly updated) app
+.\HittaDator.bat
