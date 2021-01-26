96e4dc207f1b0c2b4874c51bb76bda78223f3c27
Pavel Kuzminov <minecraftpavel@gmail.com>
2021-01-26 16:22:42 +0100

    Moved function before call

--- FindPC.ps1
+++ FindPC.ps1
@@ -3,7 +3,54 @@
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     Break
 }
+function localadmininfo {
+    param($pcName)
 
+    function Find-ADObjects($domain, $class, $filter, $attributes = "distinguishedName") {
+        $dc = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext ([System.DirectoryServices.ActiveDirectory.DirectoryContextType]"domain", $domain)
+        $dn = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($dc)
+
+        $ds = New-Object System.DirectoryServices.DirectorySearcher
+        $ds.SearchRoot = $dn.GetDirectoryEntry()
+        $ds.SearchScope = "subtree"
+        $ds.PageSize = 1024
+        $ds.Filter = "(&(objectCategory=$class)$filter)"
+        $ds.PropertiesToLoad.AddRange($attributes.Split(","))
+        $result = $ds.FindAll()
+        $ds.Dispose()
+        return $result
+    }
+
+    $adminRoles = @("CN=Kar_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Kar,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Sos_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Sos,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Lit_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Lit,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Ita_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Ita,OU=Reference,DC=gaia,DC=sll,DC=se", "CN=Dan_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Dan,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Hsf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Hsf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Lsf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Lsf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Fut_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Fut,OU=PublicTransportation,DC=gaia,DC=sll,DC=se", "CN=Int_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Int,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Trf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Trf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Sll_Wrk_LocalAdmin_SLLeKlient,OU=Workstation,OU=Groups,OU=Sll,DC=gaia,DC=sll,DC=se", "CN=Ser_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Ser,OU=Administration,DC=gaia,DC=sll,DC=se")
+    $adminRolesRegex = [string]::Join('|', $adminRoles)
+    $adVarde = (Find-ADObjects "gaia" "computer" "(cn=$pcName)" "cn,MemberOf").Properties
+    if ($adVarde.memberof -match $adminRolesRegex) {
+        $true
+    }
+    else {
+        $false
+    }
+}
+function fkarfinder {
+    param($pcName)
+
+    function Find-ADObjects($attributes = "distinguishedName") {
+        $dc = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext ([System.DirectoryServices.ActiveDirectory.DirectoryContextType]"domain", "gaia");
+        $dn = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($dc);
+
+        $ds = New-Object System.DirectoryServices.DirectorySearcher;
+        $ds.SearchRoot = $dn.GetDirectoryEntry();
+        $ds.SearchScope = "subtree";
+        $ds.PageSize = 1024;
+        $ds.Filter = "(&(objectCategory=user)(userworkstations=$pcName)(cn=F*))";
+        $ds.PropertiesToLoad.AddRange($attributes.Split(","))
+        $result = $ds.FindAll();
+        $ds.Dispose();
+        return $result;
+    }
+
+    (Find-ADObjects "cn,userworkstations").Properties
+}
 Add-Type -Assembly System.Windows.Forms
 $Form = New-Object Windows.Forms.Form 
 $Form.Width = 505
@@ -193,55 +240,3 @@ $Form.MaximizeBox = $false
 
 $Form.ShowDialog()
 
-function localadmininfo {
-    param($pcName)
-
-    function Find-ADObjects($domain, $class, $filter, $attributes = "distinguishedName") {
-        $dc = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext ([System.DirectoryServices.ActiveDirectory.DirectoryContextType]"domain", $domain)
-        $dn = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($dc)
-
-        $ds = New-Object System.DirectoryServices.DirectorySearcher
-        $ds.SearchRoot = $dn.GetDirectoryEntry()
-        $ds.SearchScope = "subtree"
-        $ds.PageSize = 1024
-        $ds.Filter = "(&(objectCategory=$class)$filter)"
-        $ds.PropertiesToLoad.AddRange($attributes.Split(","))
-        $result = $ds.FindAll()
-        $ds.Dispose()
-        return $result
-    }
-
-    $adminRoles = @("CN=Kar_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Kar,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Sos_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Sos,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Lit_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Lit,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Ita_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Ita,OU=Reference,DC=gaia,DC=sll,DC=se", "CN=Dan_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Dan,OU=HealthCare,DC=gaia,DC=sll,DC=se", "CN=Hsf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Hsf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Lsf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Lsf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Fut_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Fut,OU=PublicTransportation,DC=gaia,DC=sll,DC=se", "CN=Int_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Int,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Trf_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Trf,OU=Administration,DC=gaia,DC=sll,DC=se", "CN=Sll_Wrk_LocalAdmin_SLLeKlient,OU=Workstation,OU=Groups,OU=Sll,DC=gaia,DC=sll,DC=se", "CN=Ser_Wrk_LocalAdmin_SLLeKlient,OU=eApplication,OU=Groups,OU=Ser,OU=Administration,DC=gaia,DC=sll,DC=se")
-    $adminRolesRegex = [string]::Join('|', $adminRoles)
-    $adVarde = (Find-ADObjects "gaia" "computer" "(cn=$pcName)" "cn,MemberOf").Properties
-    if ($adVarde.memberof -match $adminRolesRegex) {
-        $true
-    }
-    else {
-        $false
-    }
-}
-function fkarfinder {
-    param($pcName)
-
-    function Find-ADObjects($attributes = "distinguishedName") {
-        $dc = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext ([System.DirectoryServices.ActiveDirectory.DirectoryContextType]"domain", "gaia");
-        $dn = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($dc);
-
-        $ds = New-Object System.DirectoryServices.DirectorySearcher;
-        $ds.SearchRoot = $dn.GetDirectoryEntry();
-        $ds.SearchScope = "subtree";
-        $ds.PageSize = 1024;
-        $ds.Filter = "(&(objectCategory=user)(userworkstations=$pcName)(cn=F*))";
-        $ds.PropertiesToLoad.AddRange($attributes.Split(","))
-        $result = $ds.FindAll();
-        $ds.Dispose();
-        return $result;
-    }
-
-    (Find-ADObjects "cn,userworkstations").Properties
-}
-
-function findPc {
-    
-}
