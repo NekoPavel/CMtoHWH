bb6d5a90f52e8601aa06e12f70458f8cbaae665b
Pavel Kuzminov <minecraftpavel@gmail.com>
2021-11-01 15:49:30 +0100

    Improved Mac, roles and models

--- GETPC_threads.ps1
+++ GETPC_threads.ps1
@@ -1,5 +1,5 @@
-﻿param([parameter(Mandatory=$true)][string]$pathToCsv,[parameter(Mandatory=$true)][string]$filename)
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){
+﻿param([parameter(Mandatory = $true)][string]$pathToCsv, [parameter(Mandatory = $true)][string]$filename)
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     break
@@ -8,8 +8,9 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 #$excludedModelsRegex = [string]::Join('|',$excludedModels)
 $PCObjects = (New-Object System.Collections.Concurrent.ConcurrentQueue[PSCustomObject])
 $findPC = {
-    $pc = $_.pcName.ToLower()
-    $continue = $false
+    $pc = $_.pcName
+    #$continue = $false
+    <#
     if ($pc.StartsWith("kar") -and ($pc.length -eq 13)) { $continue = $true }
     elseif ($pc.StartsWith("lit") -and ($pc.length -eq 13)) { $continue = $true }
     elseif ($pc.StartsWith("sos") -and ($pc.length -eq 13)) { $continue = $true }
@@ -27,14 +28,18 @@ $findPC = {
     elseif ($pc.StartsWith("ita") -and ($pc.length -eq 13)) { $continue = $true }
     elseif ($pc.StartsWith("dan") -and ($pc.length -eq 13)) { $continue = $true }
     else {}
-    if ($continue) {
-        $url = "http://sysman.sll.se/SysMan/api/Client?name=" + $pc + "&take=10&skip=0&type=0&targetActive=1"
+    #>
+    if ($pc -imatch "^[KLSHIFRTPD][AIOSNUELKRT][RTSFVLAN]((LS)|(DS))\d{8}") {
+        $url = "http://sysman.sll.se/SysMan/api/Client?name=" + $pc + "&take=1&skip=0&type=0"
         $Responce = Invoke-WebRequest -Uri $url -UseDefaultCredentials -AllowUnencryptedAuthentication -SessionVariable 'Session'
-        if (!($Responce.Content | ConvertFrom-Json).result) {}
-        elseif (($Responce.Content | ConvertFrom-Json).result) {
+        #if (!($Responce.Content | ConvertFrom-Json).result) {}
+        <#else#>if (($Responce.Content | ConvertFrom-Json).result) {
             $save = $true
             $pcName = (($Responce.Content | ConvertFrom-Json).result).Name
-            $requestBody =
+            $id = (($Responce.Content | ConvertFrom-Json).result).Id
+            #Modell
+            $model = ((($Responce.Content | ConvertFrom-Json).result).hardwareModel).Name
+            <#$requestBody =
             @{
                 UserName = ""
                 ComputerName = $pcName.ToString()
@@ -52,37 +57,45 @@ $findPC = {
             if (!$mac -or !$macColon) {
                 $save = $false
             }
-            #Modell
-            $model = ((($Responce.Content | ConvertFrom-Json).result).hardwareModel).Name
+            #>
+            $request = "http://sysman.sll.se/SysMan/api/client?id=" + $id + "&name=" + $pcName + "&assetTag=" + $pcName
+            $Responce = ((Invoke-WebRequest -Uri $request -AllowUnencryptedAuthentication -WebSession $Session).Content | ConvertFrom-Json)
+            #NEW MAC
+            [string]$macColon = [string]$Responce.macAddress
+            [string]$mac = [string]([string]$macColon -replace ":", "")
+            #Serienummer
+            [string]$serial = [string]$Responce.serialNumber
             
-            $id = (($Responce.Content | ConvertFrom-Json).result).Id
             $request = "http://sysman.sll.se/SysMan/api/Reporting/Client?clientId=" + $id
             $Responce = (Invoke-WebRequest -Uri $request -AllowUnencryptedAuthentication -WebSession $Session).Content | ConvertFrom-Json
-            #Serienummer
-            [string]$serial = [string]$Responce.serial
-            if ($serial -like "*O.E.M.*") {
+            #Serienummer Påfyllning
+            if (-not ($serial.Length -gt 0)) {
+                [string]$serial = [string]$Responce.serial
+            }
+            
+            if ($serial -like "*O.E.M.*" -or $serial.Length -lt 1) {
                 $save = $false
             }
 
             #Mer modellsaker
             foreach ($pc_model in $using:pc_modelsList) {
-                if ($pc_model.hv_typ -eq $model) {
+                if ($pc_model.hv_typ -ieq $model) {
                     $model = $pc_model.Id
                     $hardware = $pc_model.hv_category
                     if ($hardware -eq "4") {
                         foreach ($aio in $using:aioList) {
                             if ($aio -eq $pcName) {
-                                        $hardware = "6"
-                                    }
-                                }
+                                $hardware = "6"
+                            }
                         }
+                    }
                     if ($hardware -eq "6" -and $model -eq "91") {
                         $model = "90"
                     }
                     if ($model -eq "69") {
                         #Om datorn INTE finns i listan så är det en av de nya
                         foreach ($aioSerial in $using:aioSerialList69) {
-                            if (!($aioSerial -eq $serial)) {
+                            if (!($aioSerial -ieq $serial)) {
                                 $model = "106"
                             }
                         }
@@ -91,7 +104,7 @@ $findPC = {
                     if ($model -eq "107") {
                         #Om datorn INTE finns i listan så är det en av de nya
                         foreach ($aioSerial in $using:aioSerialList107) {
-                            if (!($aioSerial -eq $serial)) {
+                            if (!($aioSerial -ieq $serial)) {
                                 $model = "117"
                             }
                         }
@@ -125,53 +138,45 @@ $findPC = {
             }
             #Här händer magin för roller
             $bit = $Responce.processorArchitecture
-            $names = $Responce.collections
+            
+            
             $filteredName = "Inte hittad"
-            foreach ($name in $names) {   
-                if (($name.StartsWith("Kar_Wrk_PR") -or $name.StartsWith("Sos_Wrk_PR") -or ($name.StartsWith("Lit_Wrk_PR") -or $name.StartsWith("Dan_Wrk_PR") -or $name.StartsWith("Sll_Wrk_PR") -or $name.StartsWith("Hsf_Wrk_PR") -or $name.StartsWith("Lsf_Wrk_PR") -or $name.StartsWith("Int_Wrk_PR") -or $name.StartsWith("Fut_Wrk_PR") -or $name.StartsWith("Pnf_Wrk_PR") -or $name.StartsWith("Rev_Wrk_PR") -or $name.StartsWith("Ser_Wrk_PR") -or $name.StartsWith("Tka_Wrk_PR") -or $name.StartsWith("Trf_Wrk_PR") -or $name.StartsWith("Tsl_Wrk_PR") -or $name.StartsWith("Pnf_Wrk_PR") -or $name.StartsWith("Ita_Wrk_PR")))) {
-                    $filteredName = $name
-                    $filteredName = $filteredName.Substring(11)       
-                }
+
+            $tempName = ($Responce.collections | Where-Object { $_ -match "[A-Z][a-z]{2}_Wrk_PR" })
+            if ($tempName) {
+                $filteredName = $tempName.Substring(11)
             }
-            if ($filteredName -eq "Kiosk_PC"){
-                foreach($name in $names){
-                    if ($name.StartsWith("Gai_App_CitrixReceiverVardTerminal")){
-                        $filteredName = "Vardterminal"
-                    }
-                }
+
+
+            $tempName = ($Responce.installedApplications | Where-Object { $_.Name -match "Sll_Wrk_[A-Z][a-z]{2}_PR" })
+            if ($tempName) {
+                $filteredName = $tempName.Name.Substring(15)
             }
-            $names = $Responce.installedApplications
-            if ($filteredName -eq "Inte hittad") {
-                foreach ($name in $names) {
-                    if (($name.Name.StartsWith("Sll_Wrk_Kar_PR") -or $name.Name.StartsWith("Sll_Wrk_Sos_PR") -or $name.Name.StartsWith("Sll_Wrk_Dan_PR") -or ($name.Name.StartsWith("Sll_Wrk_Lit_PR") -or $name.Name.StartsWith("Sll_Wrk_Sll_PR") -or $name.Name.StartsWith("Sll_Wrk_Hsf_PR") -or $name.Name.StartsWith("Sll_Wrk_Lsf_PR") -or $name.Name.StartsWith("Sll_Wrk_Int_PR") -or $name.Name.StartsWith("Sll_Wrk_Fut_PR") -or $name.Name.StartsWith("Sll_Wrk_Pnf_PR") -or $name.Name.StartsWith("Sll_Wrk_Rev_PR") -or $name.Name.StartsWith("Sll_Wrk_Ser_PR") -or $name.Name.StartsWith("Sll_Wrk_Tka_PR") -or $name.Name.StartsWith("Sll_Wrk_Trf_PR") -or $name.Name.StartsWith("Sll_Wrk_Tsl_PR") -or $name.Name.StartsWith("Sll_Wrk_Pnf_PR") -or $name.Name.StartsWith("Sll_Wrk_Ita_PR")))) {
-                        $filteredName = $name.Name
-                        $filteredName = $filteredName.Substring(15)
-                    }
+            
+            if (($filteredName -eq "Kiosk_PC" ) -or ($filteredName -eq "Inte hittad")) {
+                if (($Responce.installedApplications | Where-Object { $_.Name -eq "Kar_Rol_Vardterminal-Kiosk" })) {
+                    $filteredName = "Vardterminal"
                 }
-            }
-            if (($filteredName -eq "Kiosk_PC" )-or($filteredName -eq "Inte hittad")){
-                foreach($name in $names){
-                    if ($names.Name -eq "Kar_Rol_Vardterminal-Kiosk"){
-                        $filteredName = "Vardterminal"
-                    }
+                elseif ($Responce.collections -match "Gai_App_CitrixReceiverVardTerminal") {
+                    $filteredName = "Vardterminal"
                 }
             }
             foreach ($pc_role in $using:rolesList) {
-                if ($pc_role.role -eq $filteredName) {
+                if ($pc_role.role -ieq $filteredName) {
                     $filteredName = $pc_role.Id
                 }
             }
             if (($filteredName -eq "1") -and ($bit -like "*64*")) {
                 $filteredName = "2"
             }
-            if ($filteredName -eq "Inte hittad") {
+            if ($filteredName -ieq "Inte hittad") {
                 $save = $false
             }
             #Funktionskonto
             $adVarde = &$PSScriptRoot\fkarfinder.ps1 $pcName
             if ($null -ne $adVarde.cn) { $adVarde = ($adVarde.cn -join ', ') }
             else {
-                $adVarde = "N/A"
+                $adVarde = "NEJ"
             }
             $lokaladmin = &$PSScriptRoot\localadmin.ps1 $pcName
             if ($lokaladmin) {
@@ -196,17 +201,17 @@ $findPC = {
                 #>
                 $tempQueue = $using:PCObjects
                 $tempQueue.Enqueue(<#$pcObject#>[PSCustomObject]@{
-                    Hardvara_G = $hardware
-                    Operativsystem_G = $os
-                    Modell_G = $model
-                    Roll_G = $filteredName
-                    Hardvarunamn_G = $pcName
-                    MAC_Adress_G = $mac
-                    MAC_med_Kolon_G = $macColon
-                    Serienummer_G = $serial
-                    Funktionskonto_G = $adVarde
-                    Lokal_admin_G = $lokaladmin
-                })
+                        Hardvara_G       = $hardware
+                        Operativsystem_G = $os
+                        Modell_G         = $model
+                        Roll_G           = $filteredName
+                        Hardvarunamn_G   = $pcName
+                        MAC_Adress_G     = $mac
+                        MAC_med_Kolon_G  = $macColon
+                        Serienummer_G    = $serial
+                        Funktionskonto_G = $adVarde
+                        Lokal_admin_G    = $lokaladmin
+                    })
 
             }
             Remove-Variable -Name "save"
@@ -240,8 +245,8 @@ $findPC = {
 Write-Host "Din fil kommer att sparas i" $PSScriptRoot "med namnet" $filename".csv"
 $stopWatchTotal = [System.Diagnostics.Stopwatch]::StartNew()
 #$stopWatchOuter = [System.Diagnostics.Stopwatch]::StartNew()
-$pc_modelsList = Import-Csv -Delimiter ";" -Path $PSScriptRoot\all_pc_models.csv -Header 'id','hv_typ','hv_category'
-$rolesList = Import-Csv -Delimiter ";" -Path $PSScriptRoot\roles.csv -Header 'id','role'
+$pc_modelsList = Import-Csv -Delimiter ";" -Path $PSScriptRoot\all_pc_models.csv -Header 'id', 'hv_typ', 'hv_category'
+$rolesList = Import-Csv -Delimiter ";" -Path $PSScriptRoot\roles.csv -Header 'id', 'role'
 $aioList = Import-Csv -Delimiter "," -Path $PSScriptRoot\all_touch_aio.csv -Header 'name'
 $aioSerialList69 = Import-Csv -Delimiter "," -Path $PSScriptRoot\all_old_M725s.csv -Header 'serial'
 $aioSerialList107 = Import-Csv -Delimiter "," -Path $PSScriptRoot\all_old_M75s1.csv -Header 'serial'
@@ -265,7 +270,7 @@ while ($job.State -eq "Running" -or $PCObjects.Count -gt 0) {
 #$stopWatchOuter.Restart()
 #foreach ($object in $PCObjects.ToArray()) { $object | Export-Csv -Path ($PSScriptRoot + "\" + $filename + ".csv") -NoTypeInformation -Append -Force -Delimiter ";" -Encoding UTF8 }
 #Write-Host "File saving:" $stopWatchOuter.Elapsed.TotalMilliseconds
-Write-Host "Total:" $stopWatchTotal.Elapsed.TotalMinutes
+Write-Host "Total:" $stopWatchTotal.Elapsed.TotalMinutes "minuter"
 Write-Host "Done"
 #Get-Job | Stop-Job
 #Get-Job | Remove-Job
